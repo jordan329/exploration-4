85af6e27a9dc78f855217bcffede287facaa22b4
Jordan <jordanliebman329@gmail.com>
2018-12-04 12:45:38 -0600

    some stuff working some stuff not.. keep playing make it pretty

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav>
+          <ul>
+            <li><a href="/#">Home</a></li>
+            <li><a href="/#">About</a></li>
+            <li><a href="/#">Help</a></li>
+          </ul>
+        </nav>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="box" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,6 +26,16 @@ class App extends Component {
           >
             Learn React
           </a>
+          <div className="container">
+            <ol>
+              <li className="message">one</li>
+              <li className="success">two</li>
+              <li className="error">three</li>
+              <li className="warning">four</li>
+            </ol>
+          </div>
+            <article>try me </article>
+            <aside>badaboom</aside>
         </header>
       </div>
     );
